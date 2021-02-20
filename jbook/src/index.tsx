43f9adb9eb1d951f6bef964f426ddf2ff900514c
Hiep Nguyen <hoanghiep2004hy@gmail.com>
2021-02-20 22:11:25 +0700

    Fetch npm package for creating editor online

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,17 +1,81 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import fetchPlugin from './plugins/fetch-plugin';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+    const ref = useRef<any>();
+    const iframe = useRef<any>()
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.49/esbuild.wasm',
+        });
+    };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        iframe.current.srcdoc = html;
+
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window',
+            },
+        });
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    };
+
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message', (event) => {
+                        try {
+                            eval(event.data);
+                        } catch(err) {
+                            const root = document.querySelector('#root');
+                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>'+ err +'</div>';
+                            console.error(err);
+                        }
+                    }, false)
+                </script>
+            </body>
+        </html>
+    `
+
+    return (
+        <div>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+
+            <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}></iframe>
+        </div>
+    );
+};
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
