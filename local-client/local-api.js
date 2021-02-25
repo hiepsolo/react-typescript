8fbbeb616c5abfab02b8f4006747c942917b043d
Hiep Nguyen <hoanghiep2004hy@gmail.com>
2021-02-25 17:41:09 +0700

    Add lerna to ws

---
+++ local-client/local-api.js
@@ -0,0 +1,10 @@
+// Import expres
+
+// One route
+// Serve up react production assets
+
+// Save a list of cells into a file
+
+// Load up list of cells from a file
+
+// Express server listen on port 4050
